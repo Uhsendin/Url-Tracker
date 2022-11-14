a5532bdc097098c819f0277e846a7b2841a1228f
Uhsendin <chilebr99@gmail.com>
2022-11-14 10:43:34 -0500

    Added local save on refresh

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 let myUrls = [];
 
+// changes urls to an array from a string
+let urlsLocalStorage = JSON.parse(localStorage.getItem("myUrls"))
+
+if (urlsLocalStorage) {
+  myUrls = urlsLocalStorage
+  renderUrls()
+}
+
+
 inputBtn.addEventListener("click", function () {
   myUrls.push(inputEl.value);
   inputEl.value = "";
+  // saves Urls to local storage as strings
+  localStorage.setItem("myUrls", JSON.stringify(myUrls));
   renderUrls();
+  
 });
 
 function renderUrls() {
